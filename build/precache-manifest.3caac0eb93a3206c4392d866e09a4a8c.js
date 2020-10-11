26870aa0bf4831f34dc2c7b7ab326cbadec47a06
Rishabh Mishra <rishabhmishra3321@gmail.com>
2020-10-11 21:45:14 +0530

    Created a build.

---
+++ build/precache-manifest.3caac0eb93a3206c4392d866e09a4a8c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "772bf8123823142eec24a32af476363b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab3ea43a9175f5c46d57",
+    "url": "/static/css/2.e6ac38db.chunk.css"
+  },
+  {
+    "revision": "78694fd0ea5a7adae960",
+    "url": "/static/css/main.304c9f5b.chunk.css"
+  },
+  {
+    "revision": "ab3ea43a9175f5c46d57",
+    "url": "/static/js/2.7e5d88c4.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/static/js/2.7e5d88c4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78694fd0ea5a7adae960",
+    "url": "/static/js/main.7b668755.chunk.js"
+  },
+  {
+    "revision": "ec9875811aa0f00c2e31",
+    "url": "/static/js/runtime-main.92ee656d.js"
+  }
+]);
